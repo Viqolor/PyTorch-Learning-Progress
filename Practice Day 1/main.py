4b61f95fb672b4a815f9b31bfb5e184b2c6388d3
Seungrae <seungrae0707@gmail.com>
2026-03-05 15:34:46 +0900

    Update

--- Practice Day 1/main.py
+++ Practice Day 1/main.py
@@ -16,7 +16,9 @@ from loss import get_loss
 def train_epoch(model, dataloader, device, criterion, optimizer, epoch_count):
     model.train()
     print(f"Epoch #{epoch_count}")
+    num_batches = len(dataloader)
     train_loss = 0
+    batch_loss = 0
 
     for batch, (images, labels) in enumerate(dataloader):
         images, labels = images.to(device), labels.to(device)
@@ -26,10 +28,14 @@ def train_epoch(model, dataloader, device, criterion, optimizer, epoch_count):
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
+        batch_loss += loss.item()
         
         if batch % 100 == 99:
-            print(f"Batch {batch+1}, train loss: {train_loss / 100:.8f}")
-            train_loss = 0.0
+            print(f"Batch {batch+1}, Batch Loss: {batch_loss / 100:.8f}")
+            batch_loss = 0.0
+    
+    train_loss /= num_batches
+    print(f"Training Result \n Current LR: {optimizer.param_groups[0]['lr']}, Train Loss {train_loss:.8f}")
 
 def validate(model, dataloader, device, criterion):
     size = len(dataloader.dataset)
@@ -46,24 +52,24 @@ def validate(model, dataloader, device, criterion):
 
     val_loss /= num_batches
     correct /= size
-    print(f"Validation Result \n Accuracy: {(100*correct):>.1f}%, val loss {val_loss:.8f}")
+    print(f"Validation Result \n Accuracy: {(100*correct):>.1f}%, Val Loss {val_loss:.8f}")
 
 def main():
     device = torch.device("cuda" if cuda.is_available() else "cpu")
     print(f"Using {device} device")
     
     Practice_MLP = NeuralNetwork().to(device)
+    param_count = sum(p.numel() for p in Practice_MLP.parameters())
+    print(f"Total Parameters: {param_count}")
     train_loader, test_loader = get_dataloaders(batch_size = 64)
     print(f"Training data has been loaded successfully")
     
     criterion = get_loss()
     optimizer = optim.Adam(Practice_MLP.parameters(), lr = 1e-3)
-    epoch_count = 1
 
-    for i in range(10):
+    for epoch_count in range(1, 11):
         train_epoch(Practice_MLP, train_loader, device, criterion, optimizer, epoch_count)
         validate(Practice_MLP, test_loader, device, criterion)
-        epoch_count += 1
 
 if __name__ == "__main__":
         main()
